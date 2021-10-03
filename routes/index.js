aac931f8c7fc3a89ee5b572b14947881d93d4fd6
Ricardo <r_luna99@hotmail.com>
2021-10-03 14:04:55 -0600

    Customer controller was added to the database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const customerController = require('../controllers/customer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET a list of all the customers */
+router.get('/customers', customerController.index);
 
 module.exports = router;
