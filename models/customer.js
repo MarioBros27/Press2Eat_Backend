b21351b39a4f5071000a1d870b2b3513a94488b4
Ricardo <r_luna99@hotmail.com>
2021-10-18 13:27:18 -0600

    Some associations within the customer and restaurant models were updated

--- models/customer.js
+++ models/customer.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Customer.hasMany(models.Reservation);
+      Customer.hasMany(models.Reservation, { foreignKey: 'customerId' });
     }
   };
   Customer.init({
--- models/restaurant.js
+++ models/restaurant.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Restaurant.hasMany(models.Reservation);
+      Restaurant.hasMany(models.Reservation, { foreignKey: 'restaurantId'});
     }
   };
   Restaurant.init({
